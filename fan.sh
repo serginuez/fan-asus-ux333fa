37bce549a0009d386336e84725ce88445abe25ce
Sergi Nuez <sergi@nucli.org>
2022-03-16 19:38:38 +0100

    fix for hwmon greater than 9

--- fan.sh
+++ fan.sh
@@ -9,16 +9,19 @@ threshold=60
 # Time to keep the fan on (will run at full speed)
 activetime=1
 
-#read current cpu temperature
+#
+## code
+
+#read current cpu temperature. 
 curtemp=$(/usr/bin/sensors | grep pch_cannonlake -A2 | tr -s ' ' | tail -n 1 | cut -d'+' -f2 | cut -d'.' -f1)
 
 #if it's above ${threshold}, pulse the fan for ${activetime} seconds
 if [ ${curtemp} -gt 60 ]
 then
 	#TODO: if it's already on, we should do nothing, as the most probable thing is that it's been the user manually activating it
-	echo "0" > /sys/devices/platform/asus-nb-wmi/hwmon/hwmon[0-9]/pwm1_enable
+	echo "0" > /sys/devices/platform/asus-nb-wmi/hwmon/hwmon*/pwm1_enable
 	sleep ${activetime}
-	echo "2" > /sys/devices/platform/asus-nb-wmi/hwmon/hwmon[0-9]/pwm1_enable
+	echo "2" > /sys/devices/platform/asus-nb-wmi/hwmon/hwmon*/pwm1_enable
 fi
 
 exit 0
