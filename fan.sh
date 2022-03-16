ced12c2d8805252c5e0d8065ea449f22cb083db6
Sergi Nuez <sergi@nucli.org>
2022-03-16 13:09:59 +0100

    some improvements

--- fan.sh
+++ fan.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-#read current cpu temperature
-TEMPE=$(/usr/bin/sensors | grep pch_cannonlake -A2 | tr -s ' ' | tail -n 1 | cut -d'+' -f2 | cut -d'.' -f1)
+#
+## config
 
-#if it's above 60C, pulse the fan for a second
-if [ ${TEMPE} -gt 60 ]
+# Temperature threshold in Celsius
+threshold=60
+
+# Time to keep the fan on (will run at full speed)
+activetime=1
+
+#read current cpu temperature
+curtemp=$(/usr/bin/sensors | grep pch_cannonlake -A2 | tr -s ' ' | tail -n 1 | cut -d'+' -f2 | cut -d'.' -f1)
+
+#if it's above ${threshold}, pulse the fan for ${activetime} seconds
+if [ ${curtemp} -gt 60 ]
 then
+	#TODO: if it's already on, we should do nothing, as the most probable thing is that it's been the user manually activating it
 	echo "0" > /sys/devices/platform/asus-nb-wmi/hwmon/hwmon[0-9]/pwm1_enable
-	sleep 1
-	echo "2" > /sys/devices/platform/asus-nb-wmi/hwmon/hwmon[0-9]/pwm1_enable
-else
-	#if below 60C, we make sure it's off
+	sleep ${activetime}
 	echo "2" > /sys/devices/platform/asus-nb-wmi/hwmon/hwmon[0-9]/pwm1_enable
 fi
 
